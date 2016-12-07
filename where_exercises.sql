0892d76d461b42e619f78f2101a2ff15bff08e1d
Britt Mo <brittany.dunne@utexas.edu>
2016-12-07 15:46:33 -0600

    where exercise, names with Irena', 'Vidya', or 'Maya'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
 use employees;
 
 
-select emp_no, first_name, last_name from employees where first_name OR last_name LIKE '%q%';
+select emp_no, first_name, last_name from employees where first_name IN ('Irena', 'Vidya', 'Maya');
