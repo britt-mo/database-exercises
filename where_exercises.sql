3238ec01bfaa3967787d81fe0b4ad34e58f1bea7
Britt Mo <brittany.dunne@utexas.edu>
2016-12-07 16:32:08 -0600

    employees with last name that starts and ends with 'e'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,3 @@
 use employees;
 
-select emp_no, first_name, last_name from employees where last_name LIKE "e%" OR last_name LIKE "%e";
+select emp_no, first_name, last_name from employees where last_name LIKE "e%" AND last_name LIKE "%e";
