55e51aebcfb9d67d65362da43c9cf0d34073c325
Britt Mo <brittany.dunne@utexas.edu>
2016-12-07 16:40:26 -0600

    numbers 6 & 7 in exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,5 @@
 use employees;
 
-select emp_no, first_name, last_name from employees where last_name LIKE "e%" AND last_name LIKE "%e";
+select emp_no, first_name, last_name from employees where last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+select emp_no, first_name, last_name from employees where birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
