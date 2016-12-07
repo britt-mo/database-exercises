7e3b554d271b31b9eaae73302a4ad6e5ab0d00e8
Britt Mo <brittany.dunne@utexas.edu>
2016-12-07 10:41:18 -0600

    finished albums table

---
+++ albums_seeder.sql
@@ -0,0 +1,42 @@
+USE codeup;
+
+INSERT INTO albums (artist, album, genre, release_date, sales_in_millions)
+values
+  ('Michael Jackson'  , 'Thriller'                              , 'Pop, Rock, R&B'             , 1982, 46.0),
+  ('AC/DC'            , 'Back in Black'                         , 'Heavy Rock'                 , 1980, 26.1),
+  ('Pink Floyd'       , 'Dark Side of the Moon'                 , 'Progressive Rock'           , 1973, 24.2),
+  ('Whitney Houston' , 'The Bodyguard'                         , 'R&B, Soul, Pop, Soundtrack' , 1992, 27.4),
+  ('Meat Loaf'        , 'Bat Out of Hell'                       , 'Progressive Rock, Hard Rock', 1977, 20.6),
+  ('Eagles'           ,'Their Greatest Hits'                    , 'Rock, Soft rock, Folk rock' , 1976, 32.2),
+  ('Bee Gees'         ,'Saturday Night Fever'                   , 'Disco'                      , 1977, 20.6),
+  ('Fleetwood Mac'    , 'Rumors'                                , 'Soft Rock'                  , 1977, 27.9),
+  ('Shania Twain'     , 'Come On Over'                          , 'Country, Pop'               , 1997, 29.6),
+  ('Led Zeppelin'     , 'Led Zeppelin IV'                       , 'Hard Rock, Heavy Metal'     , 1971, 29.0),
+  ('Adele'            , '21'                                    , 'Pop'                        , 2011, 25.3),
+  ('Michael Jackson'  , 'Bad'                                   , 'Pop'                        , 1987, 20.3),
+  ('Alanis Morissette','Jagged Little Pill'                     , 'Alternative Rock'           , 1995, 24.8),
+  ('Celine Dion'      , 'Falling into You'                      , 'Pop, Soft rock'             , 1996, 20.2),
+  ('The Beatles'      , 'Sgt. Pepper''s Lonely Hearts Club Band', 'Rock'                       , 1967, 13.1),
+  ('Eagles'           , 'Hotel California'                      , 'Rock, Soft rock, Folk rock' , 1976, 21.5),
+  ('Mariah Carey'     , 'Music Box'                             , 'R&B'                        , 1993, 19.0),
+  ('Michael Jackson'  , 'Dangerous'                             , 'Rock, Funk, Pop'            , 1991, 17.60),
+  ('Various Artists'  , 'Dirty Dancing'                         , 'Pop, Rock, R&B'             , 1987, 17.90),
+  ('Celine Dion'      , 'Let\'s Talk About Love'                , 'Pop, Soft Rock'             , 1997, 19.30),
+  ('The Beatles'      , '1'                                     , 'Rock'                       , 2000, 21.60),
+  ('The Beatles'      , 'Abbey Road'                            , 'Rock'                       , 1969, 14.40),
+  ('Bruce Springsteen', 'Born in the U.S.A.'                    , 'Rock'                       , 1984, 19.60),
+  ('Dire Straits'     , 'Brothers in Arms'                      , 'Rock'                       , 1985, 17.70),
+  ('James Horner'     , 'Titanic: Music from the Motion Picture', 'Soundtrack'                 , 1997, 18.10),
+  ('Madonna'          , 'The Immaculate Collection'             , 'Pop, Dance'                 , 1990, 19.40),
+  ('Metallica'        , 'Metallica'                             , 'Thrash Metal, Heavy Metal'  , 1991, 19.90),
+  ('Nirvana'          , 'Nevermind'                             , 'Grunge, Alternative Rock'   , 1991, 16.70),
+  ('Pink Floyd'       , 'The Wall'                              , 'Progressive Rock'           , 1979, 17.60),
+  ('Santana'          , 'Supernatural'                          , 'Rock'                       , 1999, 20.50),
+  ('Guns N\' Roses'   , 'Appetite for Destruction'              , 'Heavy Metal, Hard Rock'     , 1987, 21.30);
+
+select*from albums;
+
+
+
+
+
