898ec2870f7b57ab039f90c0f7b2e5bf266e0925
Britt Mo <brittany.dunne@utexas.edu>
2016-12-07 16:27:12 -0600

    first name female changed 'in' to 'or'; males

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,3 @@
 use employees;
 
-select emp_no, first_name, last_name from employees where first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+select emp_no, first_name, last_name from employees where gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
